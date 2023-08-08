2b20b699c817c92757535ca8664bf5a38fe323f3
nahowo <nahowo@naver.com>
2023-08-08 23:19:57 +0900

    수정사항 저장

--- service/views.py
+++ service/views.py
@@ -9,24 +9,17 @@ def api_service_search(request):
         with open('mymovie/config3.json') as json_file:
             config_data = json.load(json_file)
         client_serviceKey = config_data['serviceKeyDecoded']
-        # q = request.GET.get('q')
-        # encText = urllib.parse.quote("{}".format(q))
-        url = "https://api.odcloud.kr/api/gov24/v3/serviceList?page=1&perPage=10"
-        book_api_request = urllib.request.Request(url)
-        book_api_request.add_header("Authorization", "Infuser "+client_serviceKey)
-        # book_api_request.add_header("X-Naver-Client-Id",client_id)
-        # book_api_request.add_header("X-Naver-Client-Secret",client_secret)
-        response = urllib.request.urlopen(book_api_request)
+        page = '1'
+        perPage = '100'
+        url = "https://api.odcloud.kr/api/gov24/v3/serviceList?page="+page+"&perPage="+perPage
+        service_api_request = urllib.request.Request(url)
+        service_api_request.add_header("Authorization", "Infuser "+client_serviceKey)
+        response = urllib.request.urlopen(service_api_request)
         rescode = response.getcode()
         if (rescode == 200):
             response_body = response.read()
             result = json.loads(response_body.decode('utf-8'))
-            # print(result)
             datas = result.get('data')
-            # print(datas[0]['사용자구분'])
-            context = {
-                'datas': datas
-            }
         else:
             print("error code: "+rescode)
         return render(request, 'service/service.html', {'datas': datas})
